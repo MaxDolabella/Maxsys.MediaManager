ac8399ca0cdcf19bc60fcfeea1d011fb98fc0b90
Maxsys Tech <maxsystech@outlook.com>
2025-03-08 13:12:30 -0300

    SysPathToStringValueConverter

---
+++ src/MUSIC_DOMAIN/Maxsys.MediaManager.MusicContext.Infra.DataEFCore/Context/ValueConversion/SysPathToStringValueConverter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Maxsys.Core.Data.ValueConversion;
+
+internal class SysPathToStringValueConverter : ValueConverter<SysPath?, string?>
+{
+    private static string? ToDatabase(SysPath? model) => model?.ToString();
+
+    private static SysPath? FromDatabase(string? provider) => provider is not null ? new(provider) : null;
+
+    public SysPathToStringValueConverter() : base(
+        model => ToDatabase(model),
+        provider => FromDatabase(provider),
+        new ConverterMappingHints(size: 255, unicode: false))
+    { }
+}
